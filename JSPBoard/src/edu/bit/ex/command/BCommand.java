f81432a01e9cb7359ecd1fe30571673cea1b00c9
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-01-20 14:14:34 +0900

    Delete JSPBoard directory

--- JSPBoard/src/edu/bit/ex/command/BCommand.java
+++
@@ -1,8 +0,0 @@
-package edu.bit.ex.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand { // 커맨드 인터페이스
-	abstract void execute(HttpServletRequest request, HttpServletResponse response);
-}
